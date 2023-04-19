5ca2bd6ada301d8861ff5e46e62fd9eb90068c51
Soumyarian <padhysoumya098@gmail.com>
2023-04-19 15:17:20 +0530

    added scale

--- src/components/CustomImage.tsx
+++ src/components/CustomImage.tsx
@@ -15,18 +15,19 @@ export const ColorShiftMaterial = shaderMaterial(
   `
     uniform float time;
     varying vec2 vUv;
+    uniform float distanceFromCenter;
     
     varying vec3 vPosition;
     float PI = 3.141592653589793238;
 
     void main() {
-      vUv = (uv-vec2(0.5))*0.9 + vec2(0.5);
+      vUv = (uv-vec2(0.5))*(1.1 - 0.2 * distanceFromCenter * (2.0-distanceFromCenter)) + vec2(0.5);
       vec3 pos = position;
 
       pos.y += sin(PI * uv.x)*0.03;
       pos.z += sin(PI * uv.x)*0.1;
 
-      pos.y += sin(time)*0.05;
+      pos.y += sin(time)*0.08;
       vUv.y += sin(time)*0.02;
 
       gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
