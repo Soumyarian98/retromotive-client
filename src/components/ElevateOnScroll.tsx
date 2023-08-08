3230f881889ad6f32e0c510f2c8b3b27853101eb
Soumyarian <padhysoumya098@gmail.com>
2023-08-08 10:17:58 +0530

    Solved window error

--- src/components/ElevateOnScroll.tsx
+++ src/components/ElevateOnScroll.tsx
@@ -2,15 +2,16 @@ import { useScrollTrigger } from "@mui/material";
 import React from "react";
 
 interface Props {
+  window?: () => Window;
   children: React.ReactElement;
 }
 
 const ElevationScroll = (props: Props) => {
-  const { children } = props;
+  const { children, window } = props;
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-    target: window,
+    target: window ? window() : undefined,
   });
 
   return React.cloneElement(children, {
